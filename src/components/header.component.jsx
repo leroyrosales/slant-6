745bb5c3e4aa27610bcc6fa32392068da6066928
Leroy Rosales <leroyrosales@gmail.com>
2022-03-08 22:04:49 -0600

    Updates placement of logo

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className="text-center">
-        <img className="max-w-[350px] mx-auto" src={'../public/crypt-cinema-logo.gif'} alt="Crypt Cinema Club" />
+        <img className="max-w-[350px] mx-auto" src={'../crypt-cinema-logo.gif'} alt="Crypt Cinema Club" />
     </header>
   )
 }
